465d0699c9f1a41e55cb02c65a622c04e76983a6
Xavi <xdelperal@institutmvm.cat>
2024-02-23 17:56:34 +0100

    Prueba de commit 2

--- src/main/java/com/clinicamvm/clinicamontalban/HelloApplication.java
+++ src/main/java/com/clinicamvm/clinicamontalban/HelloApplication.java
@@ -1,4 +1,4 @@
-asdpackage com.clinicamvm.clinicamontalban;
+package com.clinicamvm.clinicamontalban;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hola que tal!");
+        stage.setTitle("Hola  tal!");
         stage.setScene(scene);
         stage.show();
     }
