031e92fcf0c6ca6dbec79424e1f24b5af7335827
Alejandro-Dopico <agarcia2@institutmvm.cat>
2024-02-27 23:02:45 +0100

    Login Iniciado 1.02V

--- src/main/java/com/clinicamvm/clinicamontalban/HelloApplication.java
+++ src/main/java/com/clinicamvm/clinicamontalban/HelloApplication.java
@@ -12,19 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        FXMLLoader fxmlLoader2 = new FXMLLoader(HelloApplication.class.getResource("mainPanel.fxml"));
-
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        Scene scene2 = new Scene(fxmlLoader2.load(), 320, 240);
-
-        stage.setTitle("ClinicaMontalban!");
+        Scene scene = new Scene(fxmlLoader.load(), 820, 640);
+        stage.setTitle("Login - ClinicaMontalban");
         stage.setScene(scene);
-
-        stage.show();
-
-
-        stage.setScene(scene2);
-
         stage.show();
     }
 
--- src/main/java/com/clinicamvm/clinicamontalban/HelloController.java
+++ src/main/java/com/clinicamvm/clinicamontalban/HelloController.java
@@ -6,9 +6,9 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
-
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
 }
