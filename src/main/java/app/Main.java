3d6171aca4191aedfb719c64b15dc083ad6aa56d
Alejandro-Dopico <agarcia2@institutmvm.cat>
2024-05-27 22:35:20 +0200

    Update de retoques.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,5 +1,9 @@
 package app;
 
+/**
+ * Creamos un main que llama a la APP, esto se ha hecho para al crear el .jar arregla
+ * muchos errores que conllega a problemas de inestabilidad e ejecuccion.
+ */
 public class Main {
     public static void main(String[] args) {
         App.main(args);
